e97d2983b54bd7dd5e55d7157e81d1d0e3d23833
Dr. Andreas Plagens <andreas@plagens.eu>
2021-11-07 07:37:31 +0100

    Initial Commit

---
+++ SimpleCardView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleCardView
+//
+//  Created by Dr. Andreas Plagens on 07.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleCardView/SimpleCardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCardViewApp.swift
+//  SimpleCardView
+//
+//  Created by Dr. Andreas Plagens on 07.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
