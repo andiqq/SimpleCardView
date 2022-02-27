aa704121b4f0e8ebbdb108c5690ba4404cdcf775
Dr. Andreas Plagens <andreas@plagens.eu>
2022-02-28 00:19:51 +0100

    getting rid of magic numbers

--- SimpleCardView/CardView.swift
+++ SimpleCardView/CardView.swift
@@ -14,38 +14,49 @@ struct CardView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundColor(.white)
-                                        
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: Const.cornerRadius)
+                    .foregroundColor(Const.foregroundColor)
+                
+                RoundedRectangle(cornerRadius: Const.cornerRadius)
+                    .stroke(lineWidth: Const.lineWidth)
                 
                 Text(card.suit.rawValue)
-                    .font(.system(size: geometry.size.width * 0.7))
-                    .padding(.top, geometry.size.width / 20 )
-                    
-                    
+                    .font(.system(size: geometry.size.width * Const.suitFontAdjustment))
+                    .padding(.top, geometry.size.width / Const.suitPaddingAdjustment )
+                
                 VStack {
                     HStack {
                         Text(card.value.rawValue + " ")
-                            .font(Font.custom("Baskerville", size: geometry.size.width * 0.36))
-                            .fontWeight(.medium)
-                            .padding(.horizontal, geometry.size.width / 24)
+                            .font(Font.custom(Const.customFont,
+                                              size: geometry.size.width * Const.valueFontAdjustment))
+                            .fontWeight(Const.valueFontWeight)
+                            .padding(.horizontal, geometry.size.width / Const.valuePaddingAdjustment)
                             .foregroundColor(
-                                ( card.suit == .hearts || card.suit == .diamonds) ?
-                                Color(hue: 1.0, saturation: 1.0, brightness: 0.884) : .black)
-                            
+                                ( card.suit == .hearts || card.suit == .diamonds) ? Const.redColor : .black )
+                        
                         Spacer()
                     }
-                    
-                    .padding(.leading, geometry.size.width / 80)
                     Spacer()
                 }
-                    
             }
-            .padding(geometry.size.width / 24)
-        .aspectRatio( 2/3 , contentMode: .fit)
+            .padding(geometry.size.width / Const.cardDistanceAdjustment)
+            .aspectRatio( Const.aspectRatio , contentMode: .fit)
         }
+    }
+    
+    struct Const {
+        static let cornerRadius: CGFloat = 10
+        static let foregroundColor = Color.white
+        static let lineWidth: CGFloat = 3
+        static let suitFontAdjustment = 0.7
+        static let suitPaddingAdjustment: CGFloat = 20
+        static let customFont = "Baskerville"
+        static let valueFontAdjustment = 0.36
+        static let valuePaddingAdjustment: CGFloat = 24
+        static let redColor = Color(hue: 1.0, saturation: 1.0, brightness: 0.884)
+        static let cardDistanceAdjustment: CGFloat = 24
+        static let aspectRatio = CGSize(width: 2, height: 3)
+        static let valueFontWeight = Font.Weight.medium
     }
 }
 
--- SimpleCardView/ContentView.swift
+++ SimpleCardView/ContentView.swift
@@ -11,51 +11,70 @@ struct ContentView: View {
     
     @State var numberOfCards = 0.0
     
-    var cards: [Card] {
-        CardPicker.PickSomeCards(NumberOfCards: Int(numberOfCards))
+    var randomCards: [Card] {
+        var pickedCards: [Card] = []
+        for _ in 0..<Int(numberOfCards) {
+            pickedCards.append(Card(id: pickedCards.count,
+                                    value: .allCases.randomElement()!,
+                                    suit: .allCases.randomElement()!))
+        }
+        return pickedCards
     }
     
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
             HStack {
                 Text("Select number of cards:")
-                    .font(.title2)
+                    .font(Const.sliderFont)
                     .padding([.leading])
                 Spacer()
             }
             Slider(
                 value: $numberOfCards,
-                in: 0...16,
-                step: 1,
+                in: Const.minNumberOfCards...Const.maxNumberOfCards,
+                step: Const.cardStep,
                 label: { Text("Cards") },
-                minimumValueLabel: { Text("0") },
-                maximumValueLabel: { Text("16") } )
+                minimumValueLabel: { Const.minValueLabel },
+                maximumValueLabel: { Const.maxValueLabel } )
                 .padding(.horizontal)
             
             Text("\(Int(numberOfCards))")
                 .fontWeight(.semibold)
                 .foregroundColor(.blue)
                 .font(.title2)
-                .padding(.bottom, 2.0)
+                .padding(.bottom, Const.sliderBottomPadding)
             
             GeometryReader { geometry in
                 
-                let columns = [GridItem(.adaptive(minimum: 80), spacing: 0)]
+                let columns = [GridItem(.adaptive(minimum: Const.columnMin), spacing: Const.columnSpacing)]
                 
-                LazyVGrid(columns: columns, alignment: .center, spacing: 0) {
+                LazyVGrid(columns: columns, alignment: .center, spacing: Const.vgridSpacing) {
                     
-                    ForEach(cards) { card in
+                    ForEach(randomCards) { card in
                         CardView(card: card)
-                            .aspectRatio(2/3, contentMode: .fit)
+                            .aspectRatio(Const.aspectRatio, contentMode: .fit)
                     }
-             
                 }
             }
-            .padding([.top, .leading, .trailing], 3.0)
+            .padding([.top, .leading, .trailing], Const.gridPadding)
             .background(.orange)
         }
     }
     
+    struct Const {
+        static let sliderFont = Font.title2
+        static let maxNumberOfCards = 16.0
+        static let minNumberOfCards = 0.0
+        static let cardStep = 1.0
+        static let sliderBottomPadding = 2.0
+        static let columnMin: CGFloat = 80
+        static let columnSpacing: CGFloat = 0
+        static let vgridSpacing: CGFloat = 0
+        static let aspectRatio = CGSize(width: 2, height: 3)
+        static let gridPadding = 3.0
+        static let minValueLabel = Text(String(Int(minNumberOfCards)))
+        static let maxValueLabel = Text(String(Int(minNumberOfCards)))
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -63,3 +82,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
